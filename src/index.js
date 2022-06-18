0dcaed31b48894f5ca296ce661684c26e4600a25
IDAGIZA <ruthidagiza@gmail.com>
2022-06-18 11:39:32 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let inputForm = document.getElementById("create-task-form")
+  inputForm.addEventListener('submit', createNovelTask);
 });
+const createNovelTask = function (event) {
+event.preventDefault();
+let novelTask =document.createElement('li');
+let novelTaskDescription = document.getElementById("new-task-description");
+let newTaskList = document.getElementById("tasks")
+novelTask.innerText = novelTaskDescription.value;
+appendNovelTask(novelTask);
+event.target.reset()
+
+};
+
+const appendNovelTask =(task) => {
+  document.getElementById("tasks").appendChild(task);
+};
+
+
+
+
+
